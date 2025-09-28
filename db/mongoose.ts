f4de28ed76651c8fad95e4a933c0357ca38ffa0d
Jatin Ahluwalia <jatin.ahluwalia.5@gmail.com>
2025-09-29 02:19:30 +0530

    db setup

---
+++ db/mongoose.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const { MONGODB_URI } = process.env;
+
+declare global {
+  var mongooseCache: {
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
+  };
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+  cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDB = async () => {
+  if (!MONGODB_URI) throw new Error("MONGODB_URI must be set within .env");
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI, { bufferCommands: false });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log(`Connected to DB | ${process.env.NODE_ENV} - ${MONGODB_URI}`);
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+};
